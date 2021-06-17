e33b49f26d28ab62c38d0af1b161135153d19481
Wietse Muizelaar <wmuizelaar@bol.com>
2021-06-17 08:56:46 +0200

    Add printLn's to figure out which resource is causing the event loop:

--- pkg/handler/enqueue_owner.go
+++ pkg/handler/enqueue_owner.go
@@ -59,6 +59,8 @@ type EnqueueRequestForOwner struct {
 
 // Create implements EventHandler
 func (e *EnqueueRequestForOwner) Create(evt event.CreateEvent, q workqueue.RateLimitingInterface) {
+	fmt.Println("Create: object/version:", evt.Object.GetObjectKind().GroupVersionKind())
+	fmt.Println("Create: namespace:", evt.Meta.GetNamespace(), "name:", evt.Meta.GetName())
 	for _, req := range e.getOwnerReconcileRequest(evt.Meta) {
 		q.Add(req)
 	}
@@ -66,6 +68,10 @@ func (e *EnqueueRequestForOwner) Create(evt event.CreateEvent, q workqueue.RateL
 
 // Update implements EventHandler
 func (e *EnqueueRequestForOwner) Update(evt event.UpdateEvent, q workqueue.RateLimitingInterface) {
+	fmt.Println("Update: old object/version:", evt.ObjectOld.GetObjectKind().GroupVersionKind())
+	fmt.Println("Update: old namespace:", evt.MetaOld.GetNamespace(), "name:", evt.MetaOld.GetName())
+	fmt.Println("Update: new object/version:", evt.ObjectNew.GetObjectKind().GroupVersionKind())
+	fmt.Println("Update: new namespace:", evt.MetaNew.GetNamespace(), "name:", evt.MetaNew.GetName())
 	for _, req := range e.getOwnerReconcileRequest(evt.MetaOld) {
 		q.Add(req)
 	}
@@ -76,6 +82,8 @@ func (e *EnqueueRequestForOwner) Update(evt event.UpdateEvent, q workqueue.RateL
 
 // Delete implements EventHandler
 func (e *EnqueueRequestForOwner) Delete(evt event.DeleteEvent, q workqueue.RateLimitingInterface) {
+	fmt.Println("Delete: object/version:", evt.Object.GetObjectKind().GroupVersionKind())
+	fmt.Println("Delete: namespace:", evt.Meta.GetNamespace(), "name:", evt.Meta.GetName())
 	for _, req := range e.getOwnerReconcileRequest(evt.Meta) {
 		q.Add(req)
 	}
@@ -83,6 +91,8 @@ func (e *EnqueueRequestForOwner) Delete(evt event.DeleteEvent, q workqueue.RateL
 
 // Generic implements EventHandler
 func (e *EnqueueRequestForOwner) Generic(evt event.GenericEvent, q workqueue.RateLimitingInterface) {
+	fmt.Println("Generic: object/version:", evt.Object.GetObjectKind().GroupVersionKind())
+	fmt.Println("Generic: namespace:", evt.Meta.GetNamespace(), "name:", evt.Meta.GetName())
 	for _, req := range e.getOwnerReconcileRequest(evt.Meta) {
 		q.Add(req)
 	}
